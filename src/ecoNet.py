dfda570a82bf8096012786fa31206d8cd0617f1a
Zach Quinlan <Zquinlan@gmail.com>
2021-05-10 19:45:40 -0700

    Pull library, analog and canopus from GNPS then get inchikeys is working

--- src/ecoNet.py
+++ src/ecoNet.py
@@ -1,12 +1,6 @@
 import requests
-import argparse
 import json
 import pandas as pd
-
-parser = argparse.ArgumentParser()
-parser.add_argument('task', help='GNPS Job ID')
-parser.add_argument('type', help='library or nap')
-args = parser.parse_args()
 
 class getJob:
     def __init__(self, jobID, jobType):
@@ -15,21 +9,43 @@ class getJob:
         # Requesting tsv output from GNPS API
         self.id = jobID
 
-        if jobType == 'nap':
-            url = str('http://proteomics2.ucsd.edu/ProteoSAFe/DownloadResultFile?task=' + self.id + '&block=main&file=final_out/node_attributes_table.tsv') 
-            self.df = pd.read_csv(url, sep = '\t') # Download tsv from GNPS
+        if jobType == 'canopus':
+            url = str('https://gnps.ucsd.edu/ProteoSAFe/result_json.jsp?task=' + self.id + '&view=canopus_summary') 
         if jobType == 'library':
             url = str('https://gnps.ucsd.edu/ProteoSAFe/result_json.jsp?task=' + self.id + '&view=view_all_clusters_withID')
-            self.df = pd.read_json(url)
+        if jobType == 'analog':
+            url = str('https://gnps.ucsd.edu/ProteoSAFe/result_json.jsp?task=' + self.id + '&view=view_all_analog_annotations_DB')
 
-        print(self.df)
+        # request JSON files from GNPS, clean and load into pandas
+        self.request = requests.get(url) # Download JSON from GNPS
+        self.clean = self.request.text.replace('{ "blockData" : [', '[').replace('] }', ']').strip()
+        self.df = pd.read_json(self.clean)
 
-         
+class getInchiKeys:
+    def __init__(self, inchis):
+        super().__init__()
 
-        # with open('test_nap.json', 'w') as f:
-        #     json.dump(self.json, f)
+        self.inchiSeries = inchis
+        # self.inchiSeries = self.inchis['INCHI'].to_list()
 
-getJob(args.task, args.type)
+        host = "http://www.chemspider.com"
+        getstring = "/InChI.asmx/InChIToInChIKey?inchi="
 
-#nap = ef8f40d00aa94e9b91aad6b00258ffc6
-#gnps = d7373e2add2a4e72a871a60c96fbbbc3
+
+        self.inchikeys = []
+
+        for i in self.inchiSeries:
+            r = requests.get('{}{}{}'.format(host, getstring, i))
+            if r.ok:
+                res = str(r.text.replace('<?xml version="1.0" encoding="utf-8"?>\r\n<string xmlns="http://www.chemspider.com/">', '').replace('</string>', '').strip())
+                self.inchikeys.append(res)
+            else:
+                self.inchikeys.append('NA')
+
+        self.inchiFrame = self.inchiSeries.to_frame()
+        self.inchiFrame['inchikey'] = self.inchikeys
+
+
+
+
+
