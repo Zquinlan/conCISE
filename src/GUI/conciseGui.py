d97b7a45cc31c98503997d1cf000282f726b245d
Zach Quinlan <Zquinlan@gmail.com>
2022-08-30 18:41:05 -0700

    added subdirectories for command line tool and gui. Added workflow file for gui

---
+++ src/GUI/conciseGui.py
@@ -0,0 +1,249 @@
+import sys, os, subprocess
+import qdarkstyle
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from queue import Queue
+from workflowConciseGui import *
+
+#File search widget
+class fileSearch(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)
+        self.searchDirectory = QLineEdit(self)
+        self.searchDirectory.setFixedWidth(300)
+        self.searchDirectory.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.findButton = QPushButton("Select File", self)
+        self.findButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.findButton.clicked.connect(self.dirClick)
+
+        layout.addWidget(self.searchDirectory, 0)
+        layout.addWidget(self.findButton, 2)
+        layout.setAlignment(Qt.AlignLeft)
+
+    @pyqtSlot()
+    def dirClick(self):
+        # currentDirectory = self.searchDirectory.text()
+        fname = QFileDialog.getOpenFileName(self, 'Select a File', '','All files (*.*)')[0] # Needs to select a file and not a folder
+
+        if fname:
+            # Returns pathName with the '/' separators converted to separators that are appropriate for the underlying operating system.
+            # On Windows, toNativeSeparators("c:/winnt/system32") returns
+            # "c:\winnt\system32".
+            fname = QDir.toNativeSeparators(fname)
+
+        if os.path.isfile(fname):
+            self.searchDirectory.setText(fname)
+
+#Directory search widget
+class dirSearch(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)
+        self.searchDirectory = QLineEdit(self)
+        self.searchDirectory.setFixedWidth(300)
+        self.searchDirectory.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.findButton = QPushButton("Select Directory", self)
+        self.findButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.findButton.clicked.connect(self.dirClick)
+
+        layout.addWidget(self.searchDirectory, 0)
+        layout.addWidget(self.findButton, 2)
+        layout.setAlignment(Qt.AlignLeft)
+
+    @pyqtSlot()
+    def dirClick(self):
+        currentDirectory = self.searchDirectory.text()
+        fname = QFileDialog.getExistingDirectory(self, 'Select a Directory', currentDirectory)
+
+        if fname:
+            # Returns pathName with the '/' separators converted to separators that are appropriate for the underlying operating system.
+            # On Windows, toNativeSeparators("c:/winnt/system32") returns
+            # "c:\winnt\system32".
+            fname = QDir.toNativeSeparators(fname)
+
+        if os.path.isdir(fname):
+            self.searchDirectory.setText(fname)
+
+
+## This is the meain gui
+class mainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = 'ConCISE'
+
+        if sys.platform == "linux" or sys.platform == "linux2":
+            self.system = "linux"
+        elif sys.platform == "darwin":
+            self.system = "darwin"
+        elif sys.platform == "win32" or sys.platform == "win64":
+            self.system = "windows"
+
+
+        if self.system == 'linux':
+            self.key = "Ctrl"
+        elif self.system == 'darwin':
+            self.key = "Cmnd"
+        elif self.system == 'windows':
+            self.key = "Ctrl"
+
+        # self.createMenuBar()
+        self.initUI()
+
+
+    def initUI(self):
+        def resource_path(relative_path):
+         if hasattr(sys, '_MEIPASS'):
+             return os.path.join(sys._MEIPASS, relative_path)
+         return os.path.join(os.path.abspath("."), relative_path)
+
+        # Setting Window Geometry
+        self.setWindowTitle(self.title)
+        self.setGeometry(0, 0, 600, 500)
+        self.statusBar().showMessage('Ready')
+
+        #Defining scroll area
+        scroll = QScrollArea()
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.setAlignment(Qt.AlignTop)
+
+        # Adding Widgets
+        self.toplabel = QLabel(self)
+        self.toplabel.setFont(QFont('Arial', 20))
+        self.toplabel.setText("ConCISE: Consensus Classifications of In Silico Elucidations")
+        layout.addWidget(self.toplabel)
+
+
+        # Task ID Title:
+        self.taskIdlabel = QLabel(self)
+        self.taskIdlabel.setFont(QFont('Arial', 15))
+        self.taskIdlabel.setText("GNPS Task ID:")
+        layout.addWidget(self.taskIdlabel)
+
+        #Task ID line Edit
+        self.taskId = QLineEdit(self)
+        self.taskId.setPlaceholderText("Enter your GNPS task ID")
+        self.taskId.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        layout.addWidget(self.taskId)
+
+        #Adding file selectors
+        #Label for canopus
+        self.canopusLabel = QLabel(self)
+        self.canopusLabel.setFont(QFont('Arial', 15))
+        self.canopusLabel.setText("CANOPUS summary file")
+        self.canopusLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        #Canopus file line edit
+        self.canopus = fileSearch()
+        self.canopus.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        #Network info label
+        self.networkLabel = QLabel(self)
+        self.networkLabel.setFont(QFont('Arial', 15))
+        self.networkLabel.setText("Network info tsv")
+        self.networkLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        #Network file line edit
+        self.network = fileSearch()
+        self.network.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        #Export label
+        self.exportLabel = QLabel(self)
+        self.exportLabel.setFont(QFont('Arial', 15))
+        self.exportLabel.setText("Export directory")
+        self.exportLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        #Export line edit
+        self.export = dirSearch()
+        self.export.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        
+        #Adding all the widgets
+        layout.addWidget(self.canopusLabel)
+        layout.addWidget(self.canopus)
+        layout.addWidget(self.networkLabel)
+        layout.addWidget(self.network)
+        layout.addWidget(self.exportLabel)
+        layout.addWidget(self.export)
+
+        # Make website button
+        self.mkConsensus = QPushButton("Build Consensus", self)
+        self.mkConsensus.clicked.connect(self.makeClick)
+        self.mkConsensus.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        layout.addWidget(self.mkConsensus)
+
+        #Setting scroll area as central widget
+        scroll.setWidget(widget)
+        scroll.setWidgetResizable(True)
+        self.setCentralWidget(scroll)
+
+        self.textedit = QTextEdit()
+        layout.addWidget(self.textedit)
+
+
+    
+
+    @pyqtSlot()
+    def quit(self):
+        sys.exit()
+
+    def makeClick(self):
+        task = self.taskId.text()
+        canopusFile = self.canopus.searchDirectory.text()
+        networkFile = self.network.searchDirectory.text()
+        exportDir = self.export.searchDirectory.text()
+
+        mkErr = "None!"
+
+        ##Errors out if no files are selected
+        #Task ID Erorr
+        if task == None:
+            mkErr = 'no task ID'
+            message = QMessageBox.question(self, "Error", "No GNPS task ID provided", QMessageBox.Cancel, QMessageBox.Cancel)
+
+        #Canopus file error
+        if not os.path.isfile(canopusFile):
+
+            mkErr = "no canopus selected"
+            message = QMessageBox.question(self, "Error", "No canopus file selected", QMessageBox.Cancel, QMessageBox.Cancel)
+
+        #network file error
+        if not os.path.isfile(networkFile):
+
+            mkErr = "no network selected"
+            message = QMessageBox.question(self, "Error", "No network file selected", QMessageBox.Cancel, QMessageBox.Cancel)
+
+
+        if mkErr == 'None!':
+            workflowRun(task, canopusFile, networkFile, exportDir)
+
+            # output = sys.stdout 
+            # message = QMessageBox.question(self, "Success!", "Consensuses found!!", QMessageBox.Ok, QMessageBox.Ok)
+            
+
+        self.statusBar().clearMessage()
+        self.statusBar().showMessage('Ready')
+
+
+
+
+
+
+if __name__ == '__main__':
+    # queue = Queue()
+    # sys.stdout = WriteStream(queue)
+
+    app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
+    win = mainWindow()
+    win.show()
+
+    sys.exit(app.exec_())
+
+
+
---
+++ src/GUI/workflowConciseGui.py
@@ -0,0 +1,94 @@
+# Loading in Libraries
+import requests
+import os
+import pandas as pd
+import numpy as np
+import argparse
+from conCISE import*
+
+
+class workflowRun():
+    def __init__(self, libraryID, canopusLocation, networkFile, exportDirectory):
+        ## Command line integration
+        # libraryID = libraryID
+
+
+        canopusMatch = pd.read_csv(canopusLocation, sep = '\t')
+        canopusMatch['scan'] = canopusMatch['name'].str.split('_').str[-1].astype(int)
+
+        network = pd.read_csv(networkFile, sep = '\t').rename(columns = {'cluster index': 'scan', 'componentindex': 'network'})
+
+
+        # Get jobs
+        # N/A was added to the superclass. Filter for that
+        print('Getting GNPS jobs...')
+        libraryMatch = getJob(libraryID, 'library')
+        analogMatch = None
+        # analogMatch = getJob(analogID, 'analog')  #change to if analog supplied
+        # canopusMatch = getJob(canopusID, 'canopus')
+        edgeInfo = getJob(libraryID, 'edges')
+
+        # Subset dataframes. Removed .df after libraryMatch if using verification data. 
+        librarySubset = libraryMatch.df[['#Scan#', 'superclass', 'class', 'subclass']]
+        librarySubset = librarySubset.replace('N/A', np.nan).add_suffix('_library').rename(columns={'#Scan#_library': 'scan'})
+
+        if analogMatch != None:
+            weightAnalogs = True
+            analogSubset = analogMatch.df[['#Scan#', 'superclass', 'class', 'subclass']]
+            analogSubset = analogSubset.replace('N/A', np.nan).add_suffix('_analog').rename(columns={'#Scan#_analog': 'scan'})
+
+        if analogMatch == None:
+            weightAnalogs = False
+            analogSubset = False
+        try:
+            canopusSubset = canopusMatch[['scan', 'superclass', 'class', 'subclass']].add_suffix('_canopus').rename(columns={'scan_canopus': 'scan'})
+        except:
+            canopusSubset = canopusMatch[['id', 'ClassyFire#superclass', 'ClassyFire#class', 'ClassyFire#subclass']].rename(columns = {'ClassyFire#superclass': 'superclass', 'ClassyFire#class': 'class', 'ClassyFire#subclass': 'subclass'}).add_suffix('_canopus').rename(columns={'scan_canopus': 'scan'})
+
+        networkSubset = network[['scan', 'network']]
+
+        # Merge library, analogs and 
+        print(' ')
+        print('merging annotations...')
+        # network = makeNet(edgeInfo) # This is for once I figure out the edgeInfo table IF using this change network to network.networks below
+        merged = mergeAnnotations(librarySubset, canopusSubset, networkSubset, analog = analogSubset)
+
+        print(' ')
+        print('Weighting nodes by relative cosine score...')
+        weights = None
+        # weights = weightEdges(edgeInfo) # Not all edges are in this dataframe.
+
+        # Printing to CL for user
+        print(' ')
+        print('finding consensus annotations...')
+        # Propogate annotations
+        if not weights == None:
+            annotations = selectAnnotation(merged.library, merged.insilico, networkSubset, weights.edgeWeightings, analogWeight = weightAnalogs)
+
+        if weights == None:
+            annotations = selectAnnotation(merged.library, merged.insilico, networkSubset, analogWeight = weightAnalogs, edgeWeights = None)
+
+        exportDir = exportDirectory + '/conciseConsensus.csv'
+        annotations.export.to_csv(exportDir)
+
+        # Caluculating %consensus
+        numLibrary = len(annotations.export[annotations.export['matchSource'] == 'Library'].dropna(subset = ['conciseConsensus'])[['network']].drop_duplicates())
+        totalLibrary = len(merged.library[merged.library['superclass_library'].notna() == True][['network']].drop_duplicates())
+        numInsilico = len(annotations.export[annotations.export['matchSource'] == 'In silico'].dropna(subset = ['conciseConsensus'])[['network']].drop_duplicates())
+        totalInsicilo = len(merged.insilico[merged.insilico['superclass_canopus'].notna() == True][['network']].drop_duplicates())
+
+
+        libraryConsensus = round(numLibrary/totalLibrary, 4)
+        insilicoConsensus = round(numInsilico/totalInsicilo, 4)
+        totalConsensus = round(((numLibrary + numInsilico) / (totalInsicilo + totalLibrary)), 4)
+        # Reporting success rate for consensus sequences
+        print(' ')
+        print(str(str(numLibrary) + " out of " + str(totalLibrary) + " (" + str(libraryConsensus*100) + "%)" + " networks with Library ID's found a consensus annotation"))
+        print(str(str(numInsilico) + " out of "  + str(totalInsicilo) + " (" + str(insilicoConsensus*100) + "%)" +  " networks with Insilico annotations found a consensus annotation"))
+        print(str(str(totalConsensus*100) + "%" + " of all networks with an annotation recieved a consensus annotation"))
+        print(str('conciseConsensus csv summary exported to:', exportDirectory))
+
+
+
+
+
